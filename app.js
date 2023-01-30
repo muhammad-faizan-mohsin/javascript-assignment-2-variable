92a1f10faa87408b3b63d3fcc4e05531490be603
faizan dhedhi <120604695+muhammad-faizan-mohsin@users.noreply.github.com>
2023-01-30 17:58:46 +0500

    js-assignment-2

---
+++ app.js
@@ -0,0 +1,132 @@
+// Variable Of String
+
+// Assignment
+// answer no 1
+// var userName = "faizan";
+// console.log(userName)
+
+// answer no 2
+// var myName = "Muhamad Faizan Raza";
+// console.log(myName)
+
+// answer no 3
+// var message = "Hello World";
+// alert(message)
+
+// answer no 4
+// var studentName = "Faizan";
+// alert(studentName)
+
+// var studentAges = "20 year old";
+// alert(studentAges)
+
+// var studentSkill = "Certified Mobile Application Developer";
+// alert(studentSkill)   
+
+// answer no 5
+// var  Pizza  = "PIZZA \n PIZZ \n PIZ \n PI \n p"
+// alert(Pizza)
+
+
+// answer no 6
+// var email = "example@example.com"
+// alert("My email address is " + email)
+
+
+// answer no 7
+// var book ="A smarter way to learn javascript";
+// alert(book)
+
+// answer no 8
+// var text = "Yah! I  can write HTML content through JavaScript";
+// document.write(text)
+
+
+// answer no 9
+// var emoji = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+// document.write(emoji)
+// alert(emoji)
+
+
+// Variable Of Number
+
+// answer no 1
+// var age = 15;
+// alert("I am " + age +" year old")
+
+
+// answer no 2
+// var age = 18;
+// alert("You have visited this site" + age +" times")
+
+
+// answer no 3
+// var birthYear = 2003;
+// alert("my birth year is " + birthYear)
+
+// answer no 4
+
+// var visitorName = "Faizan";
+// var quantity = 6;
+// var productName = "Shirt"
+
+// document.write(visitorName + " ordered " + quantity +" "+ productName + " on XYZ Clothing Store")
+
+
+// Variable Names: LEGAL OR ILLEGAL 
+
+// answer no 1
+// var names ="faizan" , fatherName ="Mohsin" , skill ="Web and Mobile app development";
+// alert(skill)
+
+// answer no 2
+// ILLEGAL variable names
+
+// #1
+// var var = "faizan"
+
+// #2
+// var na me = "faizan"
+
+// #3
+// var 4name ="faizan"
+
+// #4
+// var na@me = "faizan"
+
+// #5
+// var % ="Faizan Mohsin"
+
+
+// answer no 2
+
+// #1
+// var name = "faizan";
+// console.log(name)
+
+// #2
+// var fullName = "faizan mohsin";
+
+// #3
+// var name$ = "faizan";
+
+// #4
+// var na$me ="faizan";
+
+// #5
+// var myFullName = "faizan mohsin";
+
+
+// answer no 3
+
+// var b1= "number"
+// var b2="$"
+// var b3="and"
+// var b4="_"
+// var c1="letter"
+// var d1= "sensitive"
+// var e1="keyword"
+// document.write("<h1>Rules for naming JS variables</h1><br /> <p> Variable names can only contain " + b1 + ", " + b2 +" "+b3+" "+b4+". For example $my_1stVariable</p>")
+// document.write(" <p> Variables must begin with a "+c1+", "+b2+" or "+b4+". For example: $name, _name or name </p>")
+// document.write("<p>Variable names are case "+d1+"</p>")
+// document.write("<p>Variable names should not be JS "+e1+"</p>")
